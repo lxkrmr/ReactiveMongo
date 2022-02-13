2175961f850a35a50849140b7c0665e25e077296
Alex <lxkrmr@icloud.com>
2022-02-13 11:31:35 +0100

    Added configuration

--- src/main/java/com/example/reactivemongo/ReactiveMongoApplication.java
+++ src/main/java/com/example/reactivemongo/ReactiveMongoApplication.java
@@ -1,13 +1,28 @@
 package com.example.reactivemongo;
 
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+@EnableReactiveMongoRepositories
 @SpringBootApplication
-public class ReactiveMongoApplication {
+public class ReactiveMongoApplication extends AbstractReactiveMongoConfiguration {
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveMongoApplication.class, args);
     }
 
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create();
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "reactive";
+    }
 }
